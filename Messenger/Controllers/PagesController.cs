018f7c39b3bcc9bc53426fb4d223fa277760bf18
ElcinGanbarov <elchinig@code.edu.az>
2020-06-23 21:20:01 +0400

    View Added

---
+++ Messenger/Controllers/PagesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messenger.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Chat1()
+        {
+            return View();
+        }
+        public IActionResult Chat2()
+        {
+            return View();
+        }
+    }
+}
